527f4ef9517b9168d0ef11a9a2867ae260de1450
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-05-28 15:23:59 +0100

    [UPDATE] DB Update
    Update DB and Thumbs.

--- web/test.php
+++ web/test.php
@@ -4,6 +4,7 @@
 
 	$host="db.ist.utl.pt";	// o MySQL esta disponivel nesta maquina
 	$user="ist170916";	// -> substituir pelo nome de utilizador
+	$password="pw";	// -> substituir pela password dada pelo mysql_reset
 	$dbname = $user;	// a BD tem nome identico ao utilizador
 
 	$connection = new PDO("mysql:host=" . $host. ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
